b2544e214aef504c5b569bf27608e89804dd81a5
Lakshan Fernando <lakshanf@hotmail.com>
2020-09-22 14:55:11 -0700

    Add files via upload

---
+++ linux/assembly_dependency_resolver/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#cleanup script
+
+rm -r AppWithPlugin
+rm -r PluginBase
+rm -r HelloPlugin
+
+rm plugin.sln
---
+++ linux/assembly_dependency_resolver/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# script
+
+
+dotnet new console -o AppWithPlugin
+dotnet new sln
+dotnet sln add AppWithPlugin/AppWithPlugin.csproj
+
+dotnet new classlib -o PluginBase
+dotnet sln add PluginBase/PluginBase.csproj
+dotnet add AppWithPlugin/AppWithPlugin.csproj reference PluginBase/PluginBase.csproj
+
+dotnet.exe new classlib -o HelloPlugin
+dotnet.exe sln add HelloPlugin/HelloPlugin.csproj
+
+cp AppWithPlugin_PluginLoadContext.txt AppWithPlugin/PluginLoadContext.cs
+cp -f AppWithPlugin_Program.txt AppWithPlugin/Program.cs
+
+rm HelloPlugin/Class1.cs
+cp HelloPlugin_HelloCommand.txt HelloPlugin/HelloCommand.cs
+cp -f HelloPlugin_csproj.txt HelloPlugin/HelloPlugin.csproj
+
+
+rm PluginBase/Class1.cs
+cp PluginBase_ICommand.txt PluginBase/ICommand.cs
+
+#dotnet.exe publish -r linux-x64 --self-contained true
+
+dotnet.exe build
+
+#dotnet AppWithPlugin/bin/Debug/netcoreapp3.1/linux-x64/publish/AppWithPlugin.dll
